bb32aea648e53b7508b2bfe2431d0798e376850f
Rayhan Muktader <rmuktader@gmail.com>
2021-11-22 19:29:36 -0500

    comments

--- .vscode/scripts/start.sh
+++ .vscode/scripts/start.sh
@@ -7,5 +7,6 @@ then
     docker rm -f $old_containers
 fi
 
+# Make sure there is a Django container service called "web" in the docker-compose.yml file.
 docker-compose run --name ps_app -d -p 8000:8000 -p 3000:3000 web bash -c 'python -m ptvsd --host 0.0.0.0 --port 3000 --wait manage.py runserver --noreload 0.0.0.0:8000'
 sleep 2
